b6fa2d0773778f46a235820cd35a0882db1f3d05
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-02-15 18:52:47 -0600

    format with tab size 4

--- server/models/school.ts
+++ server/models/school.ts
@@ -1,66 +1,66 @@
-import { Schema, model, Document } from 'mongoose'; 
+import { Schema, model, Document } from 'mongoose';
 
 // Define the School interface
 export interface School extends Document {
-    name: string;
-    url: string;
-    website: string;
-    state: string;
-    city: string;
-    search_terms?: string;
-    color?: string;
-    district?: string;
-    principal?: string
-    address: string;
-    other?: Object;
+	name: string;
+	url: string;
+	website: string;
+	state: string;
+	city: string;
+	search_terms?: string;
+	color?: string;
+	district?: string;
+	principal?: string
+	address: string;
+	other?: Object;
 }
 
 // Create the School schema
-const schoolSchema = new Schema<School>({ 
-     name: { 
-        type: String, 
-        required: true
-     },
-     url: { 
-       type: String, 
-       required: true   
-     },
-     website: { 
-       type: String, 
-       default: ""
-     },
-     state: { 
-        type: String, 
-        required: true,
-     }, 
-     city: { 
-        type: String, 
-        required: true
-     },
-     search_terms: { 
-        type: String, 
-        default: ""
-     }, 
-     color: { 
-        type: String, 
-        default: "white"
-     },
-     district: { 
-        type: String, 
-        default: ""
-     }, 
-     principal: { 
-        type: String, 
-        default: ""
-     },
-     address: { 
-        type: String, 
-        required: true,
-     }, 
-     other: { 
-        type: Object, 
-        default: {}
-     }
+const schoolSchema = new Schema<School>({
+	name: {
+		type: String,
+		required: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	website: {
+		type: String,
+		default: ""
+	},
+	state: {
+		type: String,
+		required: true,
+	},
+	city: {
+		type: String,
+		required: true
+	},
+	search_terms: {
+		type: String,
+		default: ""
+	},
+	color: {
+		type: String,
+		default: "white"
+	},
+	district: {
+		type: String,
+		default: ""
+	},
+	principal: {
+		type: String,
+		default: ""
+	},
+	address: {
+		type: String,
+		required: true,
+	},
+	other: {
+		type: Object,
+		default: {}
+	}
 })
 
 // Export the School model
--- server/models/teacher.ts
+++ server/models/teacher.ts
@@ -2,35 +2,35 @@ import { Document, model, Schema } from 'mongoose';
 
 // Define the Teacher interface
 interface Teacher extends Document {
-    name: string;
-    school: string;
-    school_id: string;
-    courses?: string[];
-    ratings?: number[];
+	name: string;
+	school: string;
+	school_id: string;
+	courses?: string[];
+	ratings?: number[];
 }
 
 // Create the Teacher schema
 const teacherSchema = new Schema<Teacher>({
-    name: {
-        type: String,
-        required: true,
-    },
-    school: {
-        type: String,
-        required: true,
-    },
-    school_id: {
-        type: String,
-        required: true,
-    },
-    courses: {
-        type: Array,
-        default: [],
-    },
-    ratings: {
-        type: Array,
-        default: [],
-    },
+	name: {
+		type: String,
+		required: true,
+	},
+	school: {
+		type: String,
+		required: true,
+	},
+	school_id: {
+		type: String,
+		required: true,
+	},
+	courses: {
+		type: Array,
+		default: [],
+	},
+	ratings: {
+		type: Array,
+		default: [],
+	},
 });
 
 // Export the Teacher model
