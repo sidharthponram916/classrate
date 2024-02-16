c2095de22c17599ba7e30f5b4624f00a97f4e4a8
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-02-15 18:52:01 -0600

    formatted middleware with tab size 4

--- server/middleware/verify.ts
+++ server/middleware/verify.ts
@@ -7,23 +7,23 @@ import UserModel, { type User } from "~/server/models/user";
  * @returns - Error if token is invalid
  */
 export default defineEventHandler((event) => {
-    let { req } = event.node; // Get request data
-    
-    const token = req.headers.authorization; // Get token from headers
-    if (!token) return { error: "Access Denied! Authenticate!" }; // Return error if token is not provided
+	let { req } = event.node; // Get request data
 
-    // Verify token
-    jwt.verify(token, process.env.JWT_KEY, async (err: any, payload: any) => {
-        try {
-            if (err) return { error: "Invalid Token." }; // Return error if token is invalid
+	const token = req.headers.authorization; // Get token from headers
+	if (!token) return { error: "Access Denied! Authenticate!" }; // Return error if token is not provided
 
-            const user: User | null = await UserModel.findById(payload.id); // Find user by ID
-            if (!user) return { error: "You are not this user. Invalid token!" }; // Return error if user is not found
-            req.user = user; // Set user data to request
-        } catch (error) {
-            if (error instanceof jwt.JsonWebTokenError) { // Return error if token is invalid
-                return { error: error.message } // Return error message
-            }
-        }
-    });
+	// Verify token
+	jwt.verify(token, process.env.JWT_KEY, async (err: any, payload: any) => {
+		try {
+			if (err) return { error: "Invalid Token." }; // Return error if token is invalid
+
+			const user: User | null = await UserModel.findById(payload.id); // Find user by ID
+			if (!user) return { error: "You are not this user. Invalid token!" }; // Return error if user is not found
+			req.user = user; // Set user data to request
+		} catch (error) {
+			if (error instanceof jwt.JsonWebTokenError) { // Return error if token is invalid
+				return { error: error.message } // Return error message
+			}
+		}
+	});
 });
