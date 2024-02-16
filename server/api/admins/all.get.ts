e87c2236481238f9e4aa880d5a0d29f6f26d724a
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-02-15 18:58:10 -0600

    added comments to admins route
    formatted admins route with tab size 4
    
    improved error handling

--- server/api/admins/all.get.ts
+++ server/api/admins/all.get.ts
@@ -1,10 +1,18 @@
 import AdminModel, { type Admin } from "../../models/admin";
+import { Error } from "mongoose";
 
+/**
+ * Get all admins
+ * @param event - Event data
+ * @returns - Admins
+ */
 export default defineEventHandler(async (event) => {
-    try {
-        let admins: Admin[] = await AdminModel.find();
-        return admins;
-    } catch (err) {
-        return err;
-    }
+	try {
+		let admins: Admin[] = await AdminModel.find(); // Get all admins
+		return admins; // Return admins
+	} catch (err) {
+		if (err instanceof Error) {
+			return {error: err.message} // Return error message
+		}
+	}
 })
